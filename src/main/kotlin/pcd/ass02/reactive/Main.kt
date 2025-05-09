a6484ec3565a64ffe67d76eb8d41a0f27299bc8f
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-05-09 13:30:36 +0200

    refactor: use DIP

--- src/main/kotlin/pcd/ass02/reactive/Main.kt
+++ src/main/kotlin/pcd/ass02/reactive/Main.kt
@@ -1,14 +1,17 @@
 package pcd.ass02.reactive
 
 import pcd.ass02.reactive.implementation.DependencyAnalyserLibImpl
-import pcd.ass02.reactive.ui.UI
+import pcd.ass02.reactive.implementation.ParserLibImpl
+import pcd.ass02.reactive.ui.AppWindow
 import javax.swing.SwingUtilities
 
 fun main() {
   // Start with the Event Dispatch Thread (EDT)
   SwingUtilities.invokeLater {
-    val ui = UI(DependencyAnalyserLibImpl())
-    ui.setLocationRelativeTo(null)
-    ui.isVisible = true
+    val parser: ParserLib = ParserLibImpl()
+    val analyser: DependencyAnalyserLib = DependencyAnalyserLibImpl(parser)
+    val appWindow = AppWindow(analyser)
+    appWindow.setLocationRelativeTo(null)
+    appWindow.isVisible = true
   }
 }
